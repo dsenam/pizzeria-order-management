10c7802fe34b3e3a790e633a2ca883dd9a0d2e88
Douglas Sena <dsena.m@hotmail.com>
2022-04-19 18:25:15 -0300

    + input + statusbar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
+import { StatusBar} from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
 import {ThemeProvider} from 'styled-components/native'
-import theme from '@src/theme';
+import theme from './src/theme';
+
+import {SignIn} from '@screens/SignIn'
 
 
 export default function App() {
@@ -18,7 +20,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      
+      <StatusBar style='light' translucent backgroundColor='transparent'/>
+      <SignIn />
     </ThemeProvider>
   );
 }
