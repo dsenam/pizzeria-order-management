33ca46ea4a9865fc17c45fb7ff5a526f4dce05a5
Douglas Sena <dsena.m@hotmail.com>
2022-04-21 13:21:49 -0300

    + assets + signIn page

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import {Input} from '@components/Input'
-import { Container } from './styles'
-import { Button } from '@components/Button'
+import React from "react";
+
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgetPasswordButton,
+  ForgetPasswordLabel,
+} from "./styles";
+
+import brandImg from "@assets/brand.png";
+
+import { KeyboardAvoidingView, Platform } from "react-native";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
-    return (
-        <Container>
-            <Input placeholder='E-mail' type='secondary' autoCorrect={false} autoCapitalize="none"/>
-            <Input placeholder='Senha' type='secondary' secureTextEntry/>
+  return (
+    <Container>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
 
-            <Button title='Entrar' type='secondary' />
-        </Container>
-    )
+          <ForgetPasswordButton>
+              <ForgetPasswordLabel>Esqueci minha senha</ForgetPasswordLabel>
+          </ForgetPasswordButton>
+
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
+    </Container>
+  );
 }
