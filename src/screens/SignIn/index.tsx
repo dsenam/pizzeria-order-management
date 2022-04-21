156702dccce8faf3ae14dfd38398fc8e59c1fafe
Douglas Sena <dsena.m@hotmail.com>
2022-04-21 12:54:45 -0300

    + component button

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Input} from '@components/Input'
 import { Container } from './styles'
+import { Button } from '@components/Button'
 
 export function SignIn() {
     return (
         <Container>
             <Input placeholder='E-mail' type='secondary' autoCorrect={false} autoCapitalize="none"/>
             <Input placeholder='Senha' type='secondary' secureTextEntry/>
+
+            <Button title='Entrar' type='secondary' />
         </Container>
     )
 }
