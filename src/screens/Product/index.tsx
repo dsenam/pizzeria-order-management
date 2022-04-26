4de467cc91a74eb41283e07ffd6c9be8f1001057
Douglas Sena <dsena.m@hotmail.com>
2022-04-26 11:48:28 -0300

    + header product

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { Container } from './styles'
-import {Platform} from 'react-native'
+import React from "react";
+import { Container, Header, Title, DeleteLabel } from "./styles";
+import { Platform, TouchableOpacity } from "react-native";
 
 export function Product() {
-    return (
-        <Container behavior={Platform.OS === 'ios' ? 'padding': undefined}>
-            
-        </Container>
-    )
+  return (
+    <Container
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+
+        <Header>
+            <Title>Cadastrar</Title>
+
+            <TouchableOpacity>
+                <DeleteLabel>Deletar</DeleteLabel>
+            </TouchableOpacity>
+        </Header>
+    </Container>
+  );
 }
