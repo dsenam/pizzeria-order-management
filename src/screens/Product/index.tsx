5e4caa700fb0492f9e9997720566f4c2c1ef17ac
Douglas Sena <dsena.m@hotmail.com>
2022-04-27 16:41:05 -0300

    + form product

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   Container,
   Header,
@@ -6,49 +6,84 @@ import {
   DeleteLabel,
   Upload,
   PickImageButton,
+  Form,
+  InputGroup,
+  InputGroupHeader,
+  Label,
+  MaxCharacters,
 } from "./styles";
-import * as ImagePicker from 'expo-image-picker'
-import { Platform, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { Platform, TouchableOpacity, ScrollView } from "react-native";
 import { ButtonBack } from "@components/ButtonBack";
 import { Photo } from "@components/Photo";
+import { Button } from "@components/Button";
 import { InputPrice } from "@components/InputPrice";
+import { Input } from "@components/Input";
 
 export function Product() {
-  const [image , setImage] = useState('')
+  const [image, setImage] = useState("");
 
   async function handlePickerImage() {
-    const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-    if(status === 'granted') {
+    if (status === "granted") {
       const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        aspect: [4,4]
-      })
+        aspect: [4, 4],
+      });
 
-      if(!result.cancelled) {
-        setImage(result.uri)
+      if (!result.cancelled) {
+        setImage(result.uri);
       }
     }
   }
 
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-      <Header>
-        <ButtonBack />
-        <Title>Cadastrar</Title>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header>
+          <ButtonBack />
+          <Title>Cadastrar</Title>
 
-        <TouchableOpacity>
-          <DeleteLabel>Deletar</DeleteLabel>
-        </TouchableOpacity>
-      </Header>
-      <Upload>
-        <Photo uri="" />
-        <PickImageButton onPress={handlePickerImage} title="Carregar" type="secondary" />
-      </Upload>
+          <TouchableOpacity>
+            <DeleteLabel>Deletar</DeleteLabel>
+          </TouchableOpacity>
+        </Header>
 
-      <InputPrice size="P"/>
-      <InputPrice size="M"/>
-      <InputPrice size="G"/>
+        <Upload>
+          <Photo uri="" />
+          <PickImageButton
+            onPress={handlePickerImage}
+            title="Carregar"
+            type="secondary"
+          />
+        </Upload>
+
+        <Form>
+          <InputGroup>
+            <Label>Nome</Label>
+            <Input />
+          </InputGroup>
+
+          <InputGroup>
+            <InputGroupHeader>
+              <Label>Descrição</Label>
+              <MaxCharacters>0 de 60 caracteres</MaxCharacters>
+            </InputGroupHeader>
+            <Input multiline maxLength={60} style={{ height: 80 }} />
+          </InputGroup>
+
+          <InputGroup>
+            <Label>Tamanhos e Preços</Label>
+
+            <InputPrice size="P" />
+            <InputPrice size="M" />
+            <InputPrice size="G" />
+          </InputGroup>
+
+          <Button title="Cadastrar pizza" />
+        </Form>
+      </ScrollView>
     </Container>
   );
 }
