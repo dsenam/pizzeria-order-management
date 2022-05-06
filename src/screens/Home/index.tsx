5dc97b988d1033f1adb9f90ad66a0ea036e8d347
Douglas Sena <dsena.m@hotmail.com>
2022-05-06 13:17:24 -0300

    + product card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import {MaterialIcons} from '@expo/vector-icons'
-import { TouchableOpacity } from "react-native";
-import happyEmoji from '@assets/happy.png'
+import React, { useEffect, useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Alert, TouchableOpacity } from "react-native";
+import happyEmoji from "@assets/happy.png";
+import firestore from "@react-native-firebase/firestore";
 import { useTheme } from "styled-components/native";
 import {
   Container,
@@ -11,33 +12,99 @@ import {
   Greting,
   MenuHeader,
   MenuItemsNumber,
-  Title
+  Title,
 } from "./styles";
 
 import { Search } from "@components/Search";
-
+import { ProductCard, ProductProps } from "@components/ProductCard";
+import { FlatList } from "react-native-gesture-handler";
 
 export function Home() {
-const {COLORS} = useTheme()
+  const [pizzas, setPizzas] = useState<ProductProps[]>([]);
+  const [search, setSearch] = useState("");
+  const { COLORS } = useTheme();
+
+  function fetchPizzas(value: string) {
+    const formatedValue = value.toLocaleLowerCase().trim();
+
+    firestore()
+      .collection("pizzas")
+      .orderBy("name_insensitive")
+      .startAt(formatedValue)
+      .endAt(`${formatedValue}\uf8ff`)
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        setPizzas(data);
+      })
+      .catch(() =>
+        Alert.alert("Consulta", "Não foi possível realizar a consulta")
+      );
+  }
+
+  function handleSearch() {
+    fetchPizzas(search);
+  }
+
+  function handleSearchClear() {
+    setSearch("");
+    fetchPizzas("");
+  }
+
+  useEffect(() => {
+    fetchPizzas("");
+  }, []);
+
   return (
     <Container>
       <Header>
-          <Greting>
-              <GreetingEmoji source={happyEmoji} />
-              <GreetingText>Olá, Admin</GreetingText>
-          </Greting>
+        <Greting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greting>
 
-          <TouchableOpacity>
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
+      <Search
+        onChangeText={setSearch}
+        value={search}
+        onSearch={handleSearch}
+        onClear={handleSearchClear}
+      />
 
-              <MaterialIcons name="logout" color={COLORS.TITLE} size={24}/>
-          </TouchableOpacity>
-    </Header>
-    <Search onSearch={() => {}} onClear={() => {}} />
+      <MenuHeader>
+        <Title>Cardápio</Title>
+        <MenuItemsNumber>10 pizzas</MenuItemsNumber>
+      </MenuHeader>
 
-    <MenuHeader>
-      <Title>Cardápio</Title>
-      <MenuItemsNumber>10 pizzas</MenuItemsNumber>
-    </MenuHeader>
+      <FlatList
+        data={pizzas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ProductCard
+            data={{
+              id: item.id,
+              name: item.name,
+              description: item.description,
+              photo_url: item.photo_url,
+            }}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingTop: 20,
+          paddingBottom: 125,
+          marginHorizontal: 24,
+        }}
+      />
     </Container>
   );
 }
